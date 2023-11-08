7bf9b0b94447a8fcf2c4c7f4441b45d09feac104
meesha2000 <sahudeeksha2000@gmail.com>
2023-11-08 12:45:02 +0530

    Commit all the edited files related to configuration like properties and pom.xml files.

---
+++ The-Reading-Nook/src/main/java/com/mystore/onlinebookstore/The/Reading/Nook/TheReadingNookApplication.java
@@ -0,0 +1,14 @@
+package com.mystore.onlinebookstore.The.Reading.Nook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheReadingNookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheReadingNookApplication.class, args);
+		System.out.print("Welcome to My Online book store!!");
+	}
+
+}
---
+++ The-Reading-Nook/src/test/java/com/mystore/onlinebookstore/The/Reading/Nook/TheReadingNookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.onlinebookstore.The.Reading.Nook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheReadingNookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
